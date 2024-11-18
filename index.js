59d72ac79c5827d0fd536232a6ce95b680fd4b65
Mate-Aron <126141750+Mate-Aron@users.noreply.github.com>
2024-11-18 22:24:17 +0200

    na

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Helmet CSP beállítás
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        connectSrc: ["'self'", "https://car-driving-function-app.azurewebsites.net/api/command"],
+      },
+    },
+  })
+);
+
+// CORS engedélyezése
+app.use(cors());
 
 // JSON kérések feldolgozása
 app.use(express.json());
@@ -11,6 +30,13 @@ app.post('/api/command', (req, res) => {
   res.status(200).send('Command received!');
 });
 
+// Hibakezelő
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Szerver indítása
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
